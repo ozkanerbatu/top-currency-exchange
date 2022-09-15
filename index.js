e5d51dc23309eaf60cbf1d16d58d5c65340b3db8
ozkanerbatu <76597955+ozkanerbatu@users.noreply.github.com>
2022-09-15 16:03:19 +0300

    bug fix

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const axios = require("axios").default
+
 const BASE_URL = "https://api.exchangerate.host"
 const BASE_CURRENCY = "TRY"
+
 const nowYear = new Date().getFullYear()
 const mount = new Date().getMonth() + 1
 const day = new Date().getDate()
-const lastYear = 20
+
+const lastYear = 10
 
 const getCurrencies = async (date) => {
     return await axios.get(`${BASE_URL}/${date}-${mount}-${day}?base=${BASE_CURRENCY.toUpperCase()}`)
@@ -15,17 +18,17 @@ const getCurrencies = async (date) => {
         })
 }
 
-(async function findMostIncreasedCurrencyBetweenTwoYears (date) {
-    await getCurrencies(date).then( async (data) => {
-        await getCurrencies(date + 1).then((data2) => {
-            let mostIncreasedCurrency = ""
-            let mostIncreasedCurrencyValue =-Infinity
-            for (const x in data) {
-                if (Object.hasOwnProperty.call(data, x)) {
-                    for (const y in data2) {
-                        if (Object.hasOwnProperty.call(data2, y)) {
+(async function findMostIncreasedCurrencyBetweenTwoYears(date) {
+    await getCurrencies(date)
+        .then(async (data) => {
+            await getCurrencies(date + 1)
+                .then((data2) => {
+                    let mostIncreasedCurrency = ""
+                    let mostIncreasedCurrencyValue = -Infinity
+                    for (const x in data) {
+                        for (const y in data2) {
                             if (x === y) {
-                                const increase = ((data2[y] - data[x])/data[x])*100
+                                const increase = ((data2[y] - data[x]) / data[x]) * 100
                                 if (increase > mostIncreasedCurrencyValue) {
                                     mostIncreasedCurrency = x
                                     mostIncreasedCurrencyValue = increase
@@ -33,12 +36,10 @@ const getCurrencies = async (date) => {
                             }
                         }
                     }
-                }
-            }
-            console.log(`${date} - ${date + 1}  ${mostIncreasedCurrency} ${mostIncreasedCurrencyValue.toFixed(2)} ${BASE_CURRENCY} `)
+                    console.log(`${date} - ${date + 1}  ${mostIncreasedCurrency} ${mostIncreasedCurrencyValue.toFixed(2)} ${BASE_CURRENCY} `)
+                })
         })
-    })
-    if(nowYear-1 !==date){
+    if (nowYear - 1 !== date) {
         findMostIncreasedCurrencyBetweenTwoYears(date + 1)
     }
-})(nowYear-lastYear)
+})(nowYear - lastYear)
